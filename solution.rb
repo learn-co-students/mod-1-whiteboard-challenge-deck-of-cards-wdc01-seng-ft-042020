bdfe11cb30bf259e8b0f8db2f3b7e773d0f44db3
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-05-05 09:41:33 -0400

    first commit

--- solution.rb
+++ solution.rb
@@ -1,7 +1,43 @@
+require 'pry'
 class Deck
+
+    attr_reader :cards
+    
+    def initialize
+        @cards = []
+
+    end
+
+    def cards=(new_card)
+        @cards << new_card
+    end
+
+    def choose_card
+        @cards[rand(0..51)]
+    end
+
+    
 
 end
 
 class Card
 
+    @@all = []
+    attr_reader :suit, :rank, :deck
+
+    def initialize(suit, rank, deck)
+        @deck = deck
+        @suit = suit
+        @rank = rank
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
+
 end
+
+
